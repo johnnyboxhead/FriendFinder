434504fd511bad20d71426fbc558b01a33d8523e
Johnny <johnnyheath@hotmail.com>
2018-04-27 19:59:11 -0400

    Added html and js logic around routing

---
+++ app/data/friends.js
@@ -0,0 +1,10 @@
+
+
+
+$(document).ready(function(){
+    console.log("Ready!");
+})
+
+$("submit").on("click", function(){
+    console.log("It works!");
+})
---
+++ server.js
@@ -0,0 +1,28 @@
+var express = require("express");
+var bodyParser = require("body-parser");
+var path = require("path");
+
+var app = express();
+var PORT = process.env.PORT || 3000;
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.get("", function(req, res){
+    // need to insert code here for the html page
+    res.sendFile(path.join(__dirname, "app/public/home.html"));
+})
+
+app.get("/survey", function(req, res){
+    res.sendFile(path.join(__dirname, "app/public/survey.html"));
+})
+
+
+
+
+
+
+
+app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
+});
