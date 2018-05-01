265d042293f6a8f4becec09111445c45f64a9d61
Johnny <johnnyheath@hotmail.com>
2018-04-30 21:32:44 -0400

    Added the modal

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,10 +1,10 @@
-// var express = require("express");
-// var bodyParser = require("body-parser");
-// var path = require("path");
-// var app = express();
+var express = require("express");
+var bodyParser = require("body-parser");
+var path = require("path");
+var app = express();
 
-// module.exports = function(app){
-//     app.get("/api/friends", function(req, res){
-//         res.send(friends)
-//     })
-// }
+module.exports = function(app){
+    app.get("/api/friends", function(req, res){
+        res.send(friends)
+    })
+}
